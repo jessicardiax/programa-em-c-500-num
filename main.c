cd3d948ea577d1ca7470dc7a16acdf0403fe34a2
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-11-07 20:44:41 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i;
+   
+    for (i=0; i <= 500; i++){
+        if (i%2==0){
+            printf("os numeros positivos são %d\n",i);
+            
+    }
+}
+    return 0;
+
+}
